a9bc43cefaa32983ad5d44a0ef4a6ed308c65154
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-05-29 18:07:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function letterCombinations(input_digit) {
-  //Complete the function
-}
+	//Complete the function
+   let result = [];
 
-module.exports = letterCombinations;
+   let digitToLetters = ['0','1','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz'];
+	generateCombinations(input_digit, 0, "", digitToLetters, result);
+
+	function generateCombinations(digits,index,combination,digitToLetters,result) {
+	  if (index == digits.length) {
+			  result.push(combination);
+			  return;
+		  }
+  
+		  
+		  let letters = digitToLetters[digits.charAt(index) - '0'];
+
+		  for (let i = 0; i < letters.length; i++) {
+			  generateCombinations(digits, index + 1, combination + letters.charAt(i), digitToLetters, result);
+             }
+     }
+
+	 return result;
+}
+  
+  module.exports = letterCombinations;
